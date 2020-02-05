5533f7ebb8ad9d988d74d26e675a86a3ef475624
Mateus Gomes <mateusgomesc7@gmail.com>
2020-02-05 19:04:08 -0300

    First commit

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://omnistack:J8LF5N5Yqt16oZHy@cluster0-ydp2n.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Métodos HTTP: get, post, put, delete
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+server.listen(3333);
